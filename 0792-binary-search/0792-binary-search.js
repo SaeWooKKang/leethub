3a49c97c739e3a2e23d885c8853fae621d81735e
JaeYoung <87258182+SaeWooKKang@users.noreply.github.com>
2024-06-14 11:20:44 +0900

    Time: 57 ms (53.46%), Space: 52.2 MB (47.7%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    
+
+    while(right >= left) {
+        const mid = Math.round((left + right) / 2)
+
+        if (target === nums[mid]) {
+            return mid
+        }
+        if (target > nums[mid]) {
+            left = mid + 1
+
+        } else {
+            right = mid - 1          
+        }
+    }
+
+    return -1
+};
